fc3b8296c782118afd722e822a62fe9cbc304d49
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2023-04-06 16:03:09 +0300

    Update demo for 2.1.1

--- examples.py
+++ examples.py
@@ -71,23 +71,28 @@ def check_result(hash):
 def run():
     example_requests = [
         # request to server
-        '"cid":1,"uri":"http://worldtimeapi.org/api/timezone/Europe/Kiev","jsps":["/unixtime","/day_of_year"],"trims":[1,1]',
-        '"cid":1,"uri":"https://www.binance.com/api/v3/time","jsps":["/serverTime"],"trims":[4]',
-
-        # eth_gasPrice
-        '"cid":1,"uri":"geth://","jsps":["/result"],"post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_gasPrice\\\",\\\"params\\\":[],\\\"id\\\":1}"',
-
-        # eth_getBlockByNumber
-        '"cid":1,"uri":"geth://","jsps":["/result/timestamp"],"post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByNumber\\\",\\\"params\\\":[\\\"0x1234\\\",false],\\\"id\\\":1}"',
-        '"cid":1,"uri":"geth://","jsps":["/result/hash"],"post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByNumber\\\",\\\"params\\\":[\\\"0x1234\\\",false],\\\"id\\\":1}"',
-        '"cid":1,"uri":"geth://","jsps":["/result/logsBloom"],"post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByNumber\\\",\\\"params\\\":[\\\"0x1234\\\",false],\\\"id\\\":1}"',
-
-        # eth_getBlockByHash
-        '"cid":1,"uri":"geth://","jsps":["/result/timestamp"],"post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByHash\\\",\\\"params\\\":[\\\"0xaddbab0cf8f32d7954f66771b54e0794cd22ee067a4d31fb1baa6cc812c79d77\\\",false],\\\"id\\\":1}"',
+        '"cid":1,"uri":"http://worldtimeapi.org/api/timezone/Europe/Kiev","jsps":["/unixtime","/day_of_year"],"trims":[1,1],"encoding":"json"',
+        '"cid":1,"uri":"https://www.binance.com/api/v3/time","jsps":["/serverTime"],"trims":[4],"encoding":"json"',
 
         # eth_call
-        '"cid":1,"uri":"geth://","jsps":["/result"],"post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_call\\\",\\\"params\\\":[{\\\"to\\\":\\\"0xebe8efa441b9302a0d7eaecc277c09d20d684540\\\",\\\"data\\\":\\\"0x45848dfc\\\"},\\\"latest\\\"],\\\"id\\\":1}"',
+        '"cid":1,"uri":"eth://","jsps":["/result"],"encoding":"json","ethApi":"eth_call","params":[{"from":"0xCe11987ccb652688D3dC52172E3dd34E0C79318A","to":"0x08805063883845b3fA90039b672d3549D713d055","data":"0x2e64cec1","gas":"0xfffff"},"latest"]',
     ]
+
+    '''
+    # Unsupported requests for 2.1.1
+
+    # eth_gasPrice
+    '"cid":1,"uri":"geth://","jsps":["/result"],"encoding":"json","post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_gasPrice\\\",\\\"params\\\":[],\\\"id\\\":1}"',
+
+    # eth_getBlockByNumber
+    '"cid":1,"uri":"geth://","jsps":["/result/timestamp"],"encoding":"json","post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByNumber\\\",\\\"params\\\":[\\\"0x1234\\\",false],\\\"id\\\":1}"',
+    '"cid":1,"uri":"geth://","jsps":["/result/hash"],"encoding":"json","post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByNumber\\\",\\\"params\\\":[\\\"0x1234\\\",false],\\\"id\\\":1}"',
+    '"cid":1,"uri":"geth://","jsps":["/result/logsBloom"],"post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByNumber\\\",\\\"params\\\":[\\\"0x1234\\\",false],\\\"id\\\":1}"',
+
+    # eth_getBlockByHash
+    '"cid":1,"uri":"geth://","jsps":["/result/timestamp"],"encoding":"json","post":"{\\\"jsonrpc\\\":\\\"2.0\\\",\\\"method\\\":\\\"eth_getBlockByHash\\\",\\\"params\\\":[\\\"0xaddbab0cf8f32d7954f66771b54e0794cd22ee067a4d31fb1baa6cc812c79d77\\\",false],\\\"id\\\":1}"',
+    '''
+
     for request in example_requests:
         start = time.time()
         response = send_oracle_request(request)
